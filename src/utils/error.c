74f22872ac0caebe1c1f2671801e2298166caa66
Mohamed El Hamidi <mel-hami@e2r4p2.1337.ma>
2025-05-13 21:48:54 +0100

    update errore.c
    Fix Warning msges related to const char *msg

--- src/utils/error.c
+++ src/utils/error.c
@@ -6,17 +6,15 @@
 * msg: Error msg!
 * sys_error: 1 (to add system msg to the input msg)
 */
-void	exit_error(const char *msg, const int sys_error)
+void	exit_error(char *msg, const int sys_error)
 {
-	const char *err_prefix = "minishell: error: ";
-
-	ft_putstr_fd(err_prefix, STDERR_FILENO);
+	ft_putstr_fd("minishell: error: ", STDERR_FILENO);
 	ft_putstr_fd(msg, STDERR_FILENO);
 	if (sys_error)
 	{
 		ft_putstr_fd(": ", STDERR_FILENO);
 		ft_putstr_fd(strerror(errno), STDERR_FILENO);
 	}
-	ft_putchar_fd("\n", STDERR_FILENO);
+	ft_putchar_fd('\n', STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
