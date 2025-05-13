4cebc195950070c818128c24e6566d7af9a1941d
Mohamed El Hamidi <mel-hami@e2r4p2.1337.ma>
2025-05-13 20:54:46 +0100

    update structures.c
    Added more members into t_shell

--- includes/structures.h
+++ includes/structures.h
@@ -1,17 +1,26 @@
+//                     Conflict Zone
+//                git pull -> instant update -> git push
+//
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
 
 /*
 *
-* Purpose: Tracks everything the shell needs during execution.
+* t_shell: Tracks everything the shell needs during execution.
+*
 * env:            Copy of environment variables (like PATH, USER)
 * exit_status:    Tracks $? (e.g., echo $? shows the last exit code).
-*
+* is_interactive: Is the shell reading from a terminal?
+* stdin_fd:       Backup of original stdin
+* stdout_fd:      Backup of original stdout
 */
 typedef struct s_shell
 {
 	char	**env;
 	int		exit_status;
+	int		is_interactive;
+	int     stdin_fd;
+	int     stdout_fd;
 }	t_shell;
 
 #endif
