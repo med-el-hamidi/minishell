728e9b8feade2bd5e82115e59c11097984a406a1
Mohamed El Hamidi <mel-hami@e2r4p2.1337.ma>
2025-05-13 20:59:53 +0100

    update main.c
    Define init_shell

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 */
 int	main(int argc, char **argv, char **envp)
 {
-	t_shell shell;
+	t_shell	shell;
 
 	(void)argc;
 	(void)argv;
@@ -18,10 +18,29 @@ int	main(int argc, char **argv, char **envp)
 	return (shell.exit_status);
 }
 
+/*
+* init_shell: Initialize minishell
+*
+* 1. Initialize environment variables (create copy)
+* 2. Set default exit status & is_interactive
+* 3. Backup standard file descriptors
+* 4. Initialize signal handlers
+* 5. Initialize history
+*/
 void init_shell(t_shell *shell, char **envp)
 {
-    (void)shell;
-    (void)envp;
+	shell->env = copy_env(envp);
+	if (!shell->env)
+		exit_error("Failed to initialize the environment\n", 1);
+	shell->exit_status = 0;
+	shell->is_interactive = isatty(STDIN_FILENO);
+	shell->stdin_fd = dup(STDIN_FILENO);
+	shell->stdout_fd = dup(STDOUT_FILENO);
+	if (shell->stdin_fd == -1 || shell->stdout_fd == -1)
+		exit_error("Failed to backup std file descriptors", 0);
+	setup_signals();
+	//if (shell->is_interactive)
+	//	init_history();
 }
 
 void shell_loop(t_shell *shell)
