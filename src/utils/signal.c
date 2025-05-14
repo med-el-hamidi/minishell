3b1615024355b2be8eba68a5cbe22ed5010a165f
Mohamed El Hamidi <mel-hami@e2r5p4.1337.ma>
2025-05-14 17:22:00 +0100

    Add signal.c
    Signals handlers of SIGQUIT (Ctrl+\), SIGINT (Ctrl+C) and Handler of EOF (Ctrl+D)

---
+++ src/utils/signal.c
@@ -0,0 +1,32 @@
+#include "../../includes/minishell.h"
+
+static void handle_sigquit(int sig)
+{
+	(void)sig;
+	ft_putchar_fd('\a', STDOUT_FILENO);
+}
+
+static void handle_sigint(int sig)
+{
+	(void)sig;
+	ft_putchar_fd('\n', STDOUT_FILENO);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+	//g_exit_status = 130; // Set exit status (128 + SIGINT=2)
+}
+
+void	setup_signals(void)
+{
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = handle_sigquit;
+	if (sigaction(SIGQUIT, &sa, NULL) == -1)
+		exit_error("SIGQUIT (Ctrl+\\) handler failed", 1);
+	sa.sa_handler = handle_sigint;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		exit_error("SIGINT (Ctrl+C) handler failed", 1);
+	// Handle EOF (Ctrl+D)
+}
