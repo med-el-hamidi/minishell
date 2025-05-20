705bc5f9d8d5ceecfe8bb23bf22535b5e7853606
Oussama Bensarjane <obensarj@e2r5p14.1337.ma>
2025-05-20 01:18:08 +0100

    UPDATE THE HEADER
    added 4 decl functions.

--- includes/exec.h
+++ includes/exec.h
@@ -1,7 +1,11 @@
 #ifndef EXEC_H
 # define EXEC_H
 
-/*Testing ...*/
-int	pwd(void );
+
+int buildin_echo(char **argv);
+int is_buildin(char *cmd);
+int exec_buildin(t_ast *node, t_shell *shell);
+int execute_ast(t_ast *node, t_shell *shell);
+
 
 #endif
