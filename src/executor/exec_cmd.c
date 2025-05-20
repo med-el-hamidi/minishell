29d69ecefba37191ff954bf378a4b4cbdbc36676
Oussama Bensarjane <obensarj@e2r5p14.1337.ma>
2025-05-20 01:22:37 +0100

    the start of the single exec command

---
+++ src/executor/exec_cmd.c
@@ -0,0 +1,19 @@
+#include "../../includes/minishell.h"
+
+int is_buildin(char *cmd)
+{
+	return (!ft_strncmp(cmd, "echo", 5) ||
+			!ft_strncmp(cmd, "cd", 3) ||
+			!ft_strncmp(cmd, "exit", 5) ||
+			!ft_strncmp(cmd, "export", 7) ||
+			!ft_strncmp(cmd, "unset", 6) ||
+			!ft_strncmp(cmd, "env", 4));
+}
+
+int exec_buildin(t_ast *node, t_shell *shell)
+{
+	if(!ft_strncmp(node->args[0], "echo", 5))
+		return (buildin_echo(node->args));
+	// +++ i willl ad the other build ins hereee..
+	return (1);
+}
