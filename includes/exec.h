7bb70e4b09fae0582bda0754fa1edd63d3170014
Oussama Bensarjane <obensarj@e1r6p13.1337.ma>
2025-05-21 16:41:31 +0100

    UPDATED

--- includes/exec.h
+++ includes/exec.h
@@ -2,9 +2,10 @@
 # define EXEC_H
 
 
-int buildin_echo(char **argv);
-int is_buildin(char *cmd);
-int exec_buildin(t_ast *node, t_shell *shell);
+int builtin_echo(char **argv);
+int builtin_cd(char **argv);
+int is_builtin(char *cmd);
+int exec_builtins(t_ast *node, t_shell *shell);
 int execute_ast(t_ast *node, t_shell *shell);
 
 
