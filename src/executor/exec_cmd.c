d0b7ab6289e6c64d436501551588e752200a81a2
Oussama Bensarjane <obensarj@e1r6p13.1337.ma>
2025-05-21 16:47:09 +0100

    UPDATED

--- src/executor/exec_cmd.c
+++ src/executor/exec_cmd.c
@@ -1,6 +1,6 @@
 #include "../../includes/minishell.h"
 
-int is_buildin(char *cmd)
+int is_builtin(char *cmd)
 {
 	return (!ft_strncmp(cmd, "echo", 5) ||
 			!ft_strncmp(cmd, "cd", 3) ||
@@ -10,10 +10,12 @@ int is_buildin(char *cmd)
 			!ft_strncmp(cmd, "env", 4));
 }
 
-int exec_buildin(t_ast *node, t_shell *shell)
+int exec_builtins(t_ast *node, t_shell *shell)
 {
 	if(!ft_strncmp(node->args[0], "echo", 5))
-		return (buildin_echo(node->args));
+		return (builtin_echo(node->args));
+	if(!ft_strncmp(node->args[0], "cd", 3))
+		return (builtin_cd(node->args));
 	// +++ REST OF BUILDINS here...
 	return (1);
 }
