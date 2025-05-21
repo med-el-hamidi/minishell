87bc3738f1b5f48b43be507a0f62836baa3c4045
Oussama Bensarjane <obensarj@e1r6p13.1337.ma>
2025-05-21 17:05:13 +0100

    UPDATED

--- src/executor/exec_cmd.c
+++ src/executor/exec_cmd.c
@@ -2,19 +2,19 @@
 
 int is_builtin(char *cmd)
 {
-	return (!ft_strncmp(cmd, "echo", 5) ||
-			!ft_strncmp(cmd, "cd", 3) ||
-			!ft_strncmp(cmd, "exit", 5) ||
-			!ft_strncmp(cmd, "export", 7) ||
-			!ft_strncmp(cmd, "unset", 6) ||
-			!ft_strncmp(cmd, "env", 4));
+	return (!ft_strncmp(cmd, "echo", 4) ||
+			!ft_strncmp(cmd, "cd", 2) ||
+			!ft_strncmp(cmd, "exit", 4) ||
+			!ft_strncmp(cmd, "export", 6) ||
+			!ft_strncmp(cmd, "unset", 5) ||
+			!ft_strncmp(cmd, "env", 3));
 }
 
 int exec_builtins(t_ast *node, t_shell *shell)
 {
-	if(!ft_strncmp(node->args[0], "echo", 5))
+	if(!ft_strncmp(node->args[0], "echo", 4))
 		return (builtin_echo(node->args));
-	if(!ft_strncmp(node->args[0], "cd", 3))
+	if(!ft_strncmp(node->args[0], "cd", 2))
 		return (builtin_cd(node->args));
 	// +++ REST OF BUILDINS here...
 	return (1);
