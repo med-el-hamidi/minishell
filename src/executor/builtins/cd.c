093c5478565ba86610d5d3b321e7c6a909a1ff94
Oussama Bensarjane <obensarj@e1r8p2.1337.ma>
2025-05-24 22:37:54 +0100

    Update cd.c
    version 1.0 it may change

--- src/executor/builtins/cd.c
+++ src/executor/builtins/cd.c
@@ -2,4 +2,26 @@
 
 int builtin_cd(char **argv)
 {
+	char *path;
+
+	if (!argv[1] || ft_strncmp(argv[1], "~", 1) == 0)
+	{
+		path = getenv("HOME");
+		if(path == NULL)
+		{
+			ft_putstr_fd("Minishell: cd: HOME not set\n", STDERR_FILENO);
+			return (1);
+		}
+	}
+	else if (argv[1][0] == '\0')
+		return (0);
+	else
+		path = argv[1];
+	if(chdir(path) == -1)
+	{
+		perror("cd :");
+		return 1;
+	}
+	return (0);
+	
 }
