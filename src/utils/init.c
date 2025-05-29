37f9b6aa4c14faaf4485333dcc862ec97f552823
Mohamed El Hamidi <mel-hami@e2r13p1.1337.ma>
2025-05-29 19:41:29 +0100

    update init.c

--- src/utils/init.c
+++ src/utils/init.c
@@ -30,3 +30,50 @@ char	**copy_env(char **envp)
 	//increment_shell_level(new_env);
 	return (new_env);
 }
+
+/*
+*
+* init_history: Initialize history of minishell
+*
+* 1. Read existing history
+* 2. Set history file for future writes
+*
+*/
+void	init_history(t_shell *shell)
+{
+	char	*val;
+	int		n;
+
+	shell->history.count = 0;
+	shell->history.current = 0;
+	n = 0;
+	val = getenv("HISTSIZE");
+	if (val)
+		n = ft_atoi(val);
+	if (n <= 0)
+		n = HISTSIZE;
+	shell->history.histsize = n;
+	n = 0;
+	val = getenv("HISTFILESIZE");
+	if (val)
+		n = ft_atoi(val);
+	if (n <= 0)
+		n = HISTFILESIZE;
+	shell->history.histfilesize = n;
+	shell->history.entries = malloc((shell->history.histfilesize + 1) * sizeof(char *));
+	if (!shell->history.entries)
+		exit_error("History is not initialized!", 1);
+	ft_bzero(shell->history.entries, (shell->history.histfilesize + 1) * sizeof(char *));
+	load_history(shell);
+}
+
+void	init_termios(t_shell *shell)
+{
+    // Backup original terminal settings
+	tcgetattr(STDIN_FILENO, &shell->orig_termios);
+
+    // Configure new settings (e.g., disable echo control chars)
+	shell->new_termios = shell->orig_termios;
+	shell->new_termios.c_lflag &= ~(ECHOCTL);
+	tcsetattr(STDIN_FILENO, TCSANOW, &shell->new_termios);
+}
