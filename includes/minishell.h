1a056862785e52c374fa64ab8cf88b309e16a566
Mohamed El Hamidi <mel-hami@e2r13p1.1337.ma>
2025-05-29 19:46:47 +0100

    update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,18 +1,15 @@
-//                     Conflict Zone
-//                git pull -> instant update -> git push
-//
-// Hamidi: include minishell.h only in your files, your custom header files include them here but remember! look above
-//
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <unistd.h>
 # include <errno.h>
+# include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
 # include <signal.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <termios.h>
 # include "../libft/libft.h"
 # include "structures.h"
 
@@ -27,6 +24,16 @@ void	exit_error(char *msg, const int sys_error);
 /* Init functions*/
 char	**copy_env(char **envp);
 void	setup_signals(void);
+void	init_history(t_shell *shell);
+void	init_termios(t_shell *shell);
+
+/* History functions*/
+void	load_history(t_shell *shell);
+void	add_to_history(t_shell *shell, char *input);
+void	save_history(t_shell *shell);
+void	load_recent_history(char *path, t_shell *shell, int histmem_lines_c, int histfile_lines_c);
+void	load_history_fd(t_shell *shell, int	histfile_lines_c, int *skip, int fd);
+int		get_histfile_lines_count(char *path, int oflag, int perm);
 
 /* Utilities functions */
 void	free_2d_array(char **arr);
