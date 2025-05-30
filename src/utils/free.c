73058d95fd8e0b7f74062100c8232cdb45f00c90
Mohamed El Hamidi <mel-hami@e2r11p1.1337.ma>
2025-05-30 17:22:56 +0100

    update src/utils/free.c
    Add del_env

--- src/utils/free.c
+++ src/utils/free.c
@@ -11,3 +11,15 @@ void	free_2d_array(char **arr)
 		free(*ptr++);
 	free(arr);
 }
+
+void	del_env(void *v)
+{
+	t_env	*ptr;
+
+	if (!v)
+		return ;
+	ptr = (t_env *)v;
+	free(ptr->key);
+	free(ptr->value);
+	free(ptr);
+}
