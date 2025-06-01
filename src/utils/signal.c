7b09eab68e0d3fd6b2d6cb43591b29cd11037fab
Mohamed El Hamidi <mel-hami@e1r2p1.1337.ma>
2025-06-01 20:27:34 +0100

    update src/utils/signal.c

--- src/utils/signal.c
+++ src/utils/signal.c
@@ -1,5 +1,7 @@
 #include "../../includes/minishell.h"
 
+volatile sig_atomic_t	g_exit_status = 0;
+
 static void handle_sigint(int sig)
 {
 	(void)sig;
@@ -7,7 +9,7 @@ static void handle_sigint(int sig)
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
-	//g_exit_status = 130; // Set exit status (128 + SIGINT=2)
+	g_exit_status = 130;
 }
 
 void	setup_signals(void)
