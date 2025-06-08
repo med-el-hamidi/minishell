e8500a4a099e66683516b9842079a018a63ade5a
Mohamed El Hamidi <dev.elhamidi@gmail.com>
2025-06-08 16:22:47 +0100

    update lexer.h and parser.h

--- includes/parser.h
+++ includes/parser.h
@@ -1,9 +1,10 @@
 #ifndef PARSER_H
 # define PARSER_H
 
-t_ast   *new_ast_node(int type, char **args);
-void    advance_token(t_list **tokens);
-int	    is_redirection(int type);
+t_ast	*new_ast_node(int type, char **args);
+void	advance_token(t_list **tokens);
+int		is_redirection(int type);
+char	*expand_env(t_shell *shell, const char *name);
 
 t_ast	*parse_command(t_list **tokens);
 
