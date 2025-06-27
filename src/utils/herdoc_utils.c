769a2d43896aa3857628531033802a4c4bfe1f95
Mohamed El Hamidi <mel-hami@e2r8p1.1337.ma>
2025-06-27 15:32:44 +0100

    update set_herdoc_tmp_file

--- src/utils/herdoc_utils.c
+++ src/utils/herdoc_utils.c
@@ -1,16 +1,31 @@
 #include "../../includes/minishell.h"
 
+static char *gen_tmp_file()
+{
+	static	int	uniq_id = 0;
+	char		*str;
+	char		*tmp;
+	char		*tmp_file;
+
+	str = _getpid();
+	tmp_file = ft_strjoin("/tmp/minishell_herdoc_", str);
+	free(str);
+	str = ft_itoa(uniq_id++);
+	tmp = tmp_file;
+	tmp_file = ft_strjoin(tmp_file, str);
+	free(tmp);
+	free(str);
+	return (tmp_file);
+}
+
 void	set_herdoc_tmp_file(t_ast	*ast)
 {
-	char	*tmp_file;
-	char	*pid;
-	char	*input;
-	int		fd;
-	size_t	len;
+	char		*tmp_file;
+	char		*input;
+	int			fd;
+	size_t		len;
 
-	pid = _getpid();
-	tmp_file = ft_strjoin("/tmp/minishell_herdoc_", pid);
-	free(pid);
+	tmp_file = gen_tmp_file();
 	len = ft_strlen(ast->redir_file);
 	fd = open(tmp_file, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
