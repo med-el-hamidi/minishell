c2812ba387064701edb8fa7f03868338ef9edee9
Oussama Bensarjane <obensarj@e2r4p12.1337.ma>
2025-06-29 12:39:36 +0100

    imported from parser and updated here in exec

--- src/utils/init.c
+++ src/utils/init.c
@@ -1,21 +1,25 @@
 # include "../../includes/minishell.h"
 
-static void	*cleanup_init_shell(t_list	**head_env, char **split)
+static void	*cleanup_init_shell(t_list	**head_env)
 {
-	free_2d_array(split);
 	ft_lstclear(head_env, del_env);
 	return (NULL);
 }
 
-static t_env	*create_env_v(char **split)
+static t_env	*create_env_v(char *str)
 {
-	t_env *v;
+	t_env	*v;
 
+	if (!str)
+		return (NULL);
 	v = malloc(sizeof(t_env));
 	if (!v)
 		return (NULL);
-	v->key = ft_strdup(split[0]);
-	v->value = ft_strdup(split[1]);
+	v->key = ft_substr(str, 0, ft_strchr(str, '=') - str);
+	if (ft_strchr(str, '=') + 1)
+		v->value = ft_strdup(ft_strchr(str, '=') + 1);
+	else
+		v->value = NULL;
 	return (v);
 }
 
@@ -24,7 +28,6 @@ t_list	*init_env(char **envp)
 	t_list	*head_env;
 	t_list	*node_env;
 	t_env	*v;
-	char	**split;
 	int		i;
 
 	if (!envp || !*envp)
@@ -33,16 +36,12 @@ t_list	*init_env(char **envp)
 	i = -1;
 	while (envp[++i])
 	{
-		split = ft_split(envp[i], '=');
-		if (!split || !*split)
-			cleanup_init_shell(&head_env, NULL);
-		v = create_env_v(split);
+		v = create_env_v(envp[i]);
 		if (!v)
-			cleanup_init_shell(&head_env, split);
+			cleanup_init_shell(&head_env);
 		node_env = ft_lstnew(v);
 		if (!node_env)
-			cleanup_init_shell(&head_env, split);
-		free_2d_array(split);
+			cleanup_init_shell(&head_env);
 		ft_lstadd_back(&head_env, node_env);
 	}
 	// Increment SHLVL if it exists
@@ -60,11 +59,13 @@ t_list	*init_env(char **envp)
 */
 void	init_history(t_shell *shell)
 {
+	char	*path;
 	char	*val;
 	int		n;
 
 	shell->history.count = 0;
 	shell->history.current = 0;
+	shell->history.histmem_lines_c = 0;
 	n = 0;
 	val = getenv("HISTSIZE");
 	if (val)
@@ -81,9 +82,17 @@ void	init_history(t_shell *shell)
 	shell->history.histfilesize = n;
 	shell->history.entries = malloc((shell->history.histfilesize + 1) * sizeof(char *));
 	if (!shell->history.entries)
-		exit_error("History is not initialized!", 1);
+	{
+		perror("minishell: history is not initialized!");
+		return ;
+	}
 	ft_bzero(shell->history.entries, (shell->history.histfilesize + 1) * sizeof(char *));
-	load_history(shell);
+	path = get_history_path();
+	if (!path)
+		return ;
+	if (!access(path, F_OK | R_OK))
+		load_history(shell);
+	free(path);
 }
 
 void	init_termios(t_shell *shell)
