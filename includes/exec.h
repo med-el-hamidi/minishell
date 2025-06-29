2cee23c37c9a7c5a885ff79aff04a9fa3c93f25d
Oussama Bensarjane <obensarj@e2r4p12.1337.ma>
2025-06-29 20:45:01 +0100

    update

--- includes/exec.h
+++ includes/exec.h
@@ -1,6 +1,8 @@
 #ifndef EXEC_H
 # define EXEC_H
 
+#include <sys/wait.h>
+
 /* built-ins functions*/
 int		builtin_echo(char **argv);
 int		builtin_cd(char **argv, t_shell *shell);
@@ -15,12 +17,16 @@ int		is_builtin(char *cmd);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_argv_count(char **argv);
 int		ft_strisnum(char *n);
+char	*join_3(const char *s1, char *c, const char *s2);
+char    *get_cmd_path(char *cmd, t_shell *shell);
+char	**env_list_to_envp(t_list *env_list);
 void	update_iterators(t_list **prev, t_list **current);
 void	unset_node(t_list **env_list, t_list **current, t_list **prev);
 t_list	*find_env_var(t_list *env_list, char *name);
 
 /* executor main functions */
 int		exec_builtins(t_ast *node, t_shell *shell);
+int		exec_external(t_ast *node, t_shell *shell);
 int		executor(t_ast *node, t_shell *shell);
 
 /* usung parser utils functions for testing before merging*/
