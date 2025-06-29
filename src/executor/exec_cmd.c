c7fe71cca55b8e5b2696e69ec729d95fe794925c
Oussama Bensarjane <obensarj@e2r4p12.1337.ma>
2025-06-29 12:43:39 +0100

    light update

--- src/executor/exec_cmd.c
+++ src/executor/exec_cmd.c
@@ -16,16 +16,16 @@ int	exec_builtins(t_ast *node, t_shell *shell)
 	if (!ft_strcmp(node->args[0], "echo"))
 		return (builtin_echo(node->args));
 	if (!ft_strcmp(node->args[0], "cd"))
-		return (builtin_cd(node->args));
+		return (builtin_cd(node->args, shell));
 	if (!ft_strcmp(node->args[0], "exit"))
 		return (shell->exit_status/*builtin_exit(node->args)*/);
 	if (!ft_strcmp(node->args[0], "export"))
 		return (builtin_export(node->args, &(shell->env_list)));
 	if (!ft_strcmp(node->args[0], "unset"))
-		return (builtin_unset(node->args, shell->env_list));
+		return (builtin_unset(node->args, &shell->env_list));
 	if (!ft_strcmp(node->args[0], "env"))
 		return (builtin_env(shell->env_list));
 	if (!ft_strcmp(node->args[0], "pwd"))
-		return (0);//return (builtin_pwd());
+		return (builtin_pwd());
 	return (1);
 }
--- src/executor/executor.c
+++ src/executor/executor.c
@@ -10,7 +10,7 @@ int	executor(t_ast *node, t_shell *shell)
 {
 	if (!node)
 		return (1);
-	if (node->type == AST_CMD)
+	if (node->type == AST_CMD && node->args)
 	{
 		if (is_builtin(node->args[0]))
 			return (exec_builtins(node, shell));
