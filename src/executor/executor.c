766964c7050cd43b1115c593c9ab3b168f2e2698
med-el-hamidi <dev.elhamidi@gmail.com>
2025-06-30 19:44:13 +0100

    update

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,25 +1,19 @@
 #include "../../includes/minishell.h"
 
-/*
-***TO DO***
-exec_external
-exec_pipe
-exec_redirection
-*/
 int	executor(t_ast *node, t_shell *shell)
 {
 	if (!node)
 		return (1);
-	if (node->type == AST_CMD && node->args)
+	if (node->type == AST_REDIR)
+		return (exec_redirection(node, shell));
+	else if (node->type == AST_PIPE)
+		return (0);// return (exec_pipe(node, shell));
+	else if (node->type == AST_CMD && node->args)
 	{
 		if (is_builtin(node->args[0]))
 			return (exec_builtins(node, shell));
 		else
 			return (exec_external(node, shell));
 	}
-	else if (node->type == AST_PIPE)
-		return (0);// return (exec_pipe(node, shell));
-	else if (node->type == AST_REDIR)
-		return (0);//return (exec_redirection(node, shell));
 	return (1);
 }
