155633237865b8fc84d9ddd1aca7137d08903aec
Oussama Bensarjane <obensarj@e2r3p14.1337.ma>
2025-07-03 09:11:15 +0100

    add pipe part

--- src/executor/exec_pipe.c
+++ src/executor/exec_pipe.c
@@ -1,1 +1,53 @@
 #include "../../includes/minishell.h"
+
+static void	error_exit(const char *msg)
+{
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
+
+int	exec_pipe(t_ast *node, t_shell *sh)
+{
+	int		pipefd[2];
+	pid_t	child_pid[2];
+	int		status[2];
+	int		exit_status;
+
+	if (!node || !node->left || !node->right)
+		return (EXIT_FAILURE);
+
+	if (pipe(pipefd) == -1)
+		error_exit("pipe");
+	child_pid[0] = fork();
+	if (child_pid[0] == -1)
+		error_exit("fork");
+	if (child_pid[0] == 0)
+	{
+		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+			error_exit("dup2");
+		close(pipefd[0]);
+		close(pipefd[1]);
+		exit(executor(node->left, sh));
+	}
+	child_pid[1] = fork();
+	if (child_pid[1] == -1)
+		error_exit("fork");
+
+	if (child_pid[1] == 0)
+	{
+		if (dup2(pipefd[0], STDIN_FILENO) == -1)
+			error_exit("dup2");
+		close(pipefd[0]);
+		close(pipefd[1]);
+		exit(executor(node->right, sh));
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(child_pid[0], &status[0], 0);
+	waitpid(child_pid[1], &status[1], 0);
+	if (WIFEXITED(status[1]))
+		exit_status = WEXITSTATUS(status[1]);
+	else
+		exit_status = EXIT_FAILURE;
+	return (exit_status);
+}
