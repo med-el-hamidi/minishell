324b22738e61e82ba37d6237cd9aead457739f0a
Oussama Bensarjane <obensarj@e1r7p12.1337.ma>
2025-07-04 14:13:56 +0100

    Export 99.99% DONE

--- src/executor/builtins/export.c
+++ src/executor/builtins/export.c
@@ -1,41 +1,92 @@
 #include "../../../includes/minishell.h"
 
-int	builtin_export(char **argv, t_list **env_list)
+static int print_error(char *arg)
+{
+	ft_putstr_fd("miqnishell: export: `", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putendl_fd("': not a valid identifier", STDERR_FILENO);
+    return (1);
+}
+
+static int is_valid_identifier(const char *arg)
+{
+    int i = 0;
+
+    if (!arg || (!isalpha(arg[0]) && arg[0] != '_'))
+        return (0);
+    while (arg[i] && arg[i] != '=')
+    {
+        if (isalnum(arg[i]) == 0 && arg[i] != '_')
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+static int	add_to_env(char *arg, t_list **env_list)
 {
 	t_list	*existing;
-	t_env	*env_var;
+	t_env	*add_env_var_node;
 	char	*equal_pos;
-	char	*val;
-	int		argv_count;
+	char	*value;
 
+	equal_pos = ft_strchr(arg, '=');
+	if (!equal_pos)
+		return (1);//return (add_to_exported_list(arg));
+	*equal_pos = '\0';
+	value = equal_pos + 1;
+	if ((existing = find_env_var(*env_list, arg)))
+	{
+		free(((t_env *)existing->content)->value);
+		((t_env *)existing->content)->value = ft_strdup(value);
+	}
+	else
+	{
+		add_env_var_node = malloc(sizeof(t_env));
+		if (!add_env_var_node)
+			return (errno = ENOMEM,perror("malloc export add_node failed"),1);
+		add_env_var_node->key = ft_strdup(arg);
+		add_env_var_node->value = ft_strdup(value);
+		ft_lstadd_back(&(*env_list), ft_lstnew(add_env_var_node));
+	}
+	return (0);
+}
+
+static int valid_iden(char **arg, t_list **env_list)
+{
+    int i;
+    int j;
+    char **invalid_args;
+    int invalid_count;
+    int arg_counter;
+
+	invalid_count= 0;
+	arg_counter = ft_argv_count(arg);
+    invalid_args = malloc(sizeof(char *) * arg_counter);
+	i = 0;
+    if (!invalid_args)
+    	return ((errno = ENOMEM), perror("malloc export failed"), 1);
+    while ( arg[++i])
+    {
+        if (!is_valid_identifier(arg[i]))
+            invalid_args[invalid_count++] = arg[i];
+        else
+			add_to_env(arg[i], &(*env_list));
+    }
+	j = -1;
+    while (++j < invalid_count)
+        print_error(invalid_args[j]);
+    free(invalid_args);
+    return (invalid_count > 0);
+}
+
+int	builtin_export(char **argv, t_list **env_list)
+{
 	if (!argv || !env_list)
 		return (1);
 	if (!argv[1])
+		return (1);//show_expoerted_list()
+	if (valid_iden(argv, env_list))
 		return (1);
-	argv_count = ft_argv_count(argv);
-	while (--argv_count)
-	{
-		equal_pos = ft_strchr(argv[argv_count], '=');
-		if(!equal_pos)
-			return (1);
-		
-		*equal_pos = '\0';
-		val = equal_pos + 1;
-
-		if ((existing = find_env_var(*env_list, argv[argv_count])))
-		{
-			free(((t_env *)existing->content)->value);
-			((t_env *)existing->content)->value = ft_strdup(val);
-		}
-		else
-		{
-			env_var = malloc(sizeof(t_env));
-			if (!env_var)
-				return (1);
-			env_var->key = ft_strdup(argv[argv_count]);
-			env_var->value = ft_strdup(val);
-			ft_lstadd_back(&(*env_list), ft_lstnew(env_var));
-		}
-	}
 	return (0);
 }
