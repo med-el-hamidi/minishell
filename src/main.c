e39e497b1586d003d230de2395052cc2cf8ca241
Oussama Bensarjane <obensarj@e1r8p12.1337.ma>
2025-07-11 15:58:19 +0100

    exit printing handle and exit_status

--- src/main.c
+++ src/main.c
@@ -6,26 +6,26 @@
 /*   By: obensarj <obensarj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 20:47:02 by mel-hami          #+#    #+#             */
-/*   Updated: 2025/07/09 18:47:08 by obensarj         ###   ########.fr       */
+/*   Updated: 2025/06/29 21:35:02 by obensarj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-volatile sig_atomic_t g_exit_status = 0;
+volatile sig_atomic_t	g_exit_status = 0;
 
 void	init_shell(t_shell *shell, char **envp)
 {
 	shell->vars = NULL;
 	shell->vars = init_env(envp);
 	if (!shell->vars)
-		exit_error("Failed to initialize the environment\n", 1);
+		_print_error("Failed to initialize the environment\n", 0);
 	shell->exit_status = 0;
 	shell->is_interactive = (shell->is_interactive && isatty(STDIN_FILENO));
 	shell->stdin_fd = dup(STDIN_FILENO);
 	shell->stdout_fd = dup(STDOUT_FILENO);
 	if (shell->stdin_fd == -1 || shell->stdout_fd == -1)
-		exit_error("Failed to backup std file descriptors", 0);
+		_print_error("Failed to backup std file descriptors", 0);
 	setup_signals();
 	if (shell->is_interactive)
 	{
@@ -77,7 +77,8 @@ void	shell_loop(t_shell *shell)
 		if (!input)
 			break ;
 		add_to_history(shell, input);
-		shell->exit_status = g_exit_status;
+		if (g_exit_status)
+			shell->exit_status = g_exit_status;
 		tokens = lexer(shell, input);
 		free(input);
 		shell->tokens = &tokens;
@@ -88,9 +89,8 @@ void	shell_loop(t_shell *shell)
 			ast = parser(tokens);
 			if (ast)
 			{
-				// print_ast(ast, 0);//test
+				print_ast(ast, 0);//test
 				shell->exit_status = executor(ast, shell);
-				printf("exit_status %d\n", shell->exit_status);//test
 				free_ast(ast);
 			}
 			ft_lstclear(&tokens, del_token);
@@ -103,6 +103,7 @@ void	cleanup_shell(t_shell *shell)
 	if (shell->is_interactive)
 	{
 		if (shell->history.path
+			&& !access(shell->history.path, F_OK | R_OK | W_OK)
 			&& (shell->history.count - shell->history.current) > 0)
 			save_history(shell, shell->history.path);
 		free(shell->history.path);
@@ -113,8 +114,6 @@ void	cleanup_shell(t_shell *shell)
 	ft_lstclear(&shell->vars, del_env);
 	close(shell->stdin_fd);
 	close(shell->stdout_fd);
-	// if (shell->is_interactive)
-	// 	 printf("exit\n");
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -131,6 +130,8 @@ int	main(int argc, char **argv, char **envp)
 	else
 		shell_loop(&shell);
 	cleanup_shell(&shell);
+	if (shell.is_interactive)
+		printf("exit\n");
 	if (g_exit_status)
 		return (g_exit_status);
 	else
