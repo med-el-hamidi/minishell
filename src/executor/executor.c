b8ce7aadbc043715b4b1259ad51331bf9221db62
Oussama Bensarjane <obensarj@e1r8p12.1337.ma>
2025-07-11 15:53:19 +0100

    executor.c adding new test before execution for the validiti of local vars

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: obensarj <obensarj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 17:12:34 by obensarj          #+#    #+#             */
-/*   Updated: 2025/07/07 17:12:35 by obensarj         ###   ########.fr       */
+/*   Updated: 2025/07/10 01:47:13 by obensarj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	executor(t_ast *node, t_shell *shell)
 		return (exec_pipe(node, shell));
 	else if (node->type == AST_CMD && node->args)
 	{
-		if (_is_local_vars(node->args))
+		if (_is_local_vars(node))
 			return (exec_local_vars(node->args, &shell->vars));
 		else if (is_builtin(node->args[0]))
 			return (exec_builtins(node, shell));
