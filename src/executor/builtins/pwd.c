c02179f7ccd185c08c2d651d5625018bd2d7ca5d
Oussama Bensarjane <obensarj@e1r8p12.1337.ma>
2025-07-11 20:40:16 +0100

    pwd.c pwd should never fail : fixed

--- src/executor/builtins/pwd.c
+++ src/executor/builtins/pwd.c
@@ -6,21 +6,21 @@
 /*   By: obensarj <obensarj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 22:28:42 by obensarj          #+#    #+#             */
-/*   Updated: 2025/07/07 22:28:43 by obensarj         ###   ########.fr       */
+/*   Updated: 2025/07/11 20:33:50 by obensarj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-int	builtin_pwd(void)
+int	builtin_pwd(t_list *vars)
 {
 	char	*buf;
 
 	buf = getcwd(NULL, 0);
 	if (!buf)
 	{
-		perror("pwd");
-		return (1);
+		printf("%s\n", expand_env(vars, "PWD"));
+		return (0);
 	}
 	ft_putendl_fd(buf, STDOUT_FILENO);
 	free(buf);
