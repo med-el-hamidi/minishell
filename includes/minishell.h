4c80f4f39e3c0b2c907b350b827d555794a6ba98
Mohamed El Hamidi <mel-hami@e3r10p11.1337.ma>
2025-07-18 08:02:48 +0100

    the rest of 'merged' files from parser

---
+++ includes/minishell.h
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-hami <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 21:20:08 by mel-hami          #+#    #+#             */
+/*   Updated: 2025/06/28 21:20:10 by mel-hami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <errno.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <termios.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include "../libft/libft.h"
+# include "structures.h"
+# include "lexer.h"
+# include "parser.h"
+# include "exec.h"
+
+extern volatile sig_atomic_t	g_exit_status;
+
+/* Main Functions */
+void	init_shell(t_shell *shell, char **envp);
+void	shell_loop(t_shell *shell);
+void	cleanup_shell(t_shell *shell);
+
+/* Error functions*/
+void	_print_error(char *msg, const int sys_error);
+int		print_syntax_error(const char *token);
+void	open_script_err_exit(char *script, char *msg, int e);
+
+/* Init functions*/
+t_list	*init_env(char **envp);
+void	init_shell_vars(t_list **vars);
+void	setup_signals(void);
+void	init_history(t_shell *shell);
+void	init_termios(t_shell *shell);
+
+/* History functions*/
+void	set_histfile(t_shell *shell);
+void	set_default_history_sizes(t_list **vars, char *key, int n);
+int		_add_history(t_shell *shell, char *line, int i, int *skip);
+void	load_history(t_shell *shell);
+void	add_to_history(t_shell *shell, char *input);
+void	save_history(t_shell *shell, char *path);
+int		load_recent_history(char *path, t_shell *shell, int histfile_lines_c);
+void	load_hist_fd(t_shell *shell, int histfile_lines_c, int *skip, int fd);
+int		get_histfile_lines_c(char *path, int oflag, int perm);
+
+/* Free functions */
+void	free_2d_array(char **arr);
+void	del_env(void *v);
+void	del_token(void *t);
+void	free_ast(t_ast *node);
+
+/* Other utils */
+int		set_herdoc_tmp_file(t_shell *shell, char **delimiter, int parse);
+int		open_script(char *script);
+char	*_getenv(t_list *vars, char *name);
+
+#endif
