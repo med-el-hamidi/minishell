9d823083abb16421f970afb5726537e2363949d8
Oussama Bensarjane <obensarj@e3r10p13.1337.ma>
2025-07-18 06:39:45 +0100

    update execve.c
    optimize error printing 126 127 ...

--- src/executor/execve_ext/execve.c
+++ src/executor/execve_ext/execve.c
@@ -6,47 +6,44 @@
 /*   By: obensarj <obensarj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 22:40:40 by obensarj          #+#    #+#             */
-/*   Updated: 2025/07/15 23:52:46 by obensarj         ###   ########.fr       */
+/*   Updated: 2025/07/18 06:34:19 by obensarj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-static int	is_empty_or_dir_cmd(t_shell *shell, char *arg)
+static int	handle_no_path_env(t_shell *shell, char *arg)
 {
-	if (arg && !*arg)
-	{
-		shell->exit_status = 127;
-		return (ft_putendl_fd("Command '' not found", 2), 1);
-	}
-	else if (arg[0] == '/' || !strcmp(arg, "./") || !strcmp(arg, "../"))
+	char	*env;
+
+	env = expand_env(shell->vars, "PATH");
+	if (!env)
 	{
 		if (is_directory(arg))
 		{
 			shell->exit_status = 126;
-			return (execv_print_error(arg, 4), 1);
+			execve_print_error(arg, 4);
 		}
+		else
+		{
+			shell->exit_status = 127;
+			execve_print_error(arg, 2);
+		}
+		return (1);
 	}
 	return (0);
 }
 
 static void	_execve(t_ast *node, char *path, char **envp)
 {
-	char	*sh_argv[3];
-
 	signal(SIGQUIT, SIG_DFL);
 	if (execve(path, node->args, envp) == -1)
 	{
 		if (errno == ENOEXEC)
-		{
-			sh_argv[0] = "/bin/sh";
-			sh_argv[1] = path;
-			sh_argv[2] = NULL;
-			execve("/bin/sh", sh_argv, envp);
-		}
+			exit(0);
 	}
 	ft_putstr_fd("minishell: execve ", STDERR_FILENO);
-	perror(node->args[0]);
+	perror(path);
 	exit (126);
 }
 
@@ -58,6 +55,25 @@ static void	_set_exit_status(t_shell *shell, int status)
 		shell->exit_status = 128 + WTERMSIG(status);
 }
 
+static int	_execve_print_error(t_shell *shell, char *arg)
+{
+	if (is_directory(arg) && (arg[0] == '/' || \
+		(ft_strlen(arg) > 0 && arg[ft_strlen(arg) - 1] == '/') || \
+		!strncmp(arg, "./", 2) || !strncmp(arg, "../", 3)))
+	{
+		shell->exit_status = 126;
+		execve_print_error(arg, 4);
+	}
+	else if (ft_strchr(arg, '/'))
+	{
+		shell->exit_status = 127;
+		execve_print_error(arg, 2);
+	}
+	shell->exit_status = 127;
+	execve_print_error(arg, 1);
+	return (shell->exit_status);
+}
+
 int	exec_external(t_ast *node, t_shell *shell)
 {
 	pid_t	pid;
@@ -65,15 +81,13 @@ int	exec_external(t_ast *node, t_shell *shell)
 	char	*path;
 	char	**envp;
 
-	if (!node || !node->args)
+	if (!node || !node->args || !node->args[0])
 		return (0);
-	if (is_empty_or_dir_cmd(shell, node->args[0]))
+	if (handle_no_path_env(shell, node->args[0]))
 		return (shell->exit_status);
 	path = get_cmd_path(node->args[0], shell);
-	if (!path && is_directory(node->args[0]))
-		return (free(path), execv_print_error(node->args[0], 4), 126);
-	else if (!path)
-		return (free(path), 127);
+	if (!path)
+		return (_execve_print_error(shell, node->args[0]));
 	envp = env_list_to_envp(shell->vars);
 	pid = fork();
 	if (pid == -1)
