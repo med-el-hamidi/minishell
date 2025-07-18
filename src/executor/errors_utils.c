de789d35e96fa6a24d4b09a218153ca253d7c7e2
Oussama Bensarjane <obensarj@e3r10p13.1337.ma>
2025-07-18 06:38:43 +0100

    update naming

--- src/executor/errors_utils.c
+++ src/executor/errors_utils.c
@@ -6,7 +6,7 @@
 /*   By: obensarj <obensarj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 23:23:36 by obensarj          #+#    #+#             */
-/*   Updated: 2025/07/13 19:07:18 by obensarj         ###   ########.fr       */
+/*   Updated: 2025/07/18 06:28:08 by obensarj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,15 @@ int	export_print_error(char *arg)
 	return (1);
 }
 
-int	execv_print_error(char *arg, int fg)
+int	execve_print_error(char *arg, int fg)
 {
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putstr_fd(arg, STDERR_FILENO);
 	if (fg == 1)
 		ft_putendl_fd(": command not found", STDERR_FILENO);
-	if (fg == 2)
+	else if (fg == 2)
 		ft_putendl_fd(": No such file or directory", STDERR_FILENO);
-	if (fg == 4)
+	else if (fg == 4)
 		ft_putendl_fd(": Is a directory", STDERR_FILENO);
 	return (1);
 }
