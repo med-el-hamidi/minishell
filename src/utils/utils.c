fb14268cbebee08e6ed22a1b468094a8ced4d754
Mohamed El Hamidi <mel-hami@e3r10p11.1337.ma>
2025-07-18 06:34:19 +0100

    update src/utils/utils.c

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -12,9 +12,59 @@
 
 #include "../../includes/minishell.h"
 
+static int	check_buffer_binary(unsigned char *buffer, ssize_t size)
+{
+	int		non_text_count;
+	ssize_t	i;
+
+	non_text_count = 0;
+	i = 0;
+	while (i < size)
+	{
+		if (!buffer[i])
+			return (1);
+		if (!(ft_isprint(buffer[i]) || is_whitespace(buffer[i])))
+			non_text_count++;
+		i++;
+	}
+	if ((double)non_text_count / size > 0.3)
+		return (1);
+	return (0);
+}
+
+static int	is_binary_file(const char *path, mode_t mode)
+{
+	int				fd;
+	ssize_t			bytes_read;
+	unsigned char	buffer[CHUNK_SIZE];
+	int				is_binary;
+
+	is_binary = 0;
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return (-1);
+	bytes_read = read(fd, buffer, sizeof(buffer));
+	if (bytes_read <= 0)
+		return (close(fd), -1);
+	if (S_ISREG(mode))
+	{
+		while (!is_binary)
+		{
+			is_binary = check_buffer_binary(buffer, bytes_read);
+			bytes_read = read(fd, buffer, sizeof(buffer));
+			if (bytes_read <= 0)
+				return (close(fd), -1);
+		}
+	}
+	else if (!isatty(fd))
+		is_binary = check_buffer_binary(buffer, bytes_read);
+	return (close(fd), is_binary);
+}
+
 int	open_script(char *script)
 {
-	int	fd;
+	int			fd;
+	struct stat	state;
 
 	fd = open(script, O_RDONLY);
 	if (fd == -1)
@@ -24,7 +74,18 @@ int	open_script(char *script)
 			perror(" ");
 		else
 			perror(script);
+		if (!access(script, F_OK))
+			exit(126);
 		exit(127);
+	}
+	if (fstat(fd, &state) == -1)
+		(close(fd), perror(script), exit(errno));
+	else if (is_binary_file(script, state.st_mode) == 1)
+	{
+		close(fd);
+		ft_putstr_fd(script, STDERR_FILENO);
+		t_putstr_fd(": cannot execute binary file\n", STDERR_FILENO);
+		exit(126);
 	}
 	return (fd);
 }
