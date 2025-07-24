523c607c97190ee6004749f7f843076973891212
Mohamed El Hamidi <mel-hami@e3r11p1.1337.ma>
2025-07-24 12:48:31 +0100

    update lexer_utils_bonus.c

--- minishell_bonus_part/src_bonus/lexer_bonus/lexer_utils_bonus.c
+++ minishell_bonus_part/src_bonus/lexer_bonus/lexer_utils_bonus.c
@@ -38,3 +38,20 @@ int	is_whitespace(char c)
 {
 	return ((c >= 9 && c <= 13) || c == ' ');
 }
+
+void	add_token_word(t_list **tokens, char *word)
+{
+	t_token	*token;
+
+	if (!tokens || !word)
+		return ;
+	if (ft_strchr(word, '*'))
+	{
+		handle_glob(tokens, word);
+		return ;
+	}
+	token = create_token(TOKEN_WORD, word);
+	if (!token)
+		return ;
+	ft_lstadd_back(tokens, ft_lstnew(token));
+}
