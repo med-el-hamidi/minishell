4ad793dfa6367e3e8e8267b5c221397962646c65
Oussama Bensarjane <obensarj@e3r3p15.1337.ma>
2025-07-26 18:10:59 +0100

    update structure

--- includes/structures.h
+++ includes/structures.h
@@ -17,6 +17,8 @@
 # define HISTSIZE 500
 # define HISTFILESIZE 500
 # define CHUNK_SIZE 512
+# define SNTX_EXIT_STATUS 258
+# define SIGINT_EXIT_STATUS 1
 
 typedef struct s_history
 {
@@ -103,4 +105,13 @@ typedef struct s_shell
 	int				stdin_fd;
 	int				stdout_fd;
 }	t_shell;
+
+typedef struct s_lexerctx
+{
+	t_shell	*shell;
+	t_list	**tokens;
+	char	*input;
+	size_t	*i;
+	int		amb;
+}	t_lexerctx;
 #endif
