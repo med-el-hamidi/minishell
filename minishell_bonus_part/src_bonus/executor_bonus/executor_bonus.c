5299d7f68ccf7c3945374cf3b1eff952199a11ba
Oussama Bensarjane <obensarj@e3r2p12.1337.ma>
2025-07-27 16:50:14 +0100

    update fix the executor_bonus.c norminette and remove the subshell not required

--- minishell_bonus_part/src_bonus/executor_bonus/executor_bonus.c
+++ minishell_bonus_part/src_bonus/executor_bonus/executor_bonus.c
@@ -6,49 +6,24 @@
 /*   By: obensarj <obensarj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 17:12:34 by obensarj          #+#    #+#             */
-/*   Updated: 2025/07/19 11:03:19 by obensarj         ###   ########.fr       */
+/*   Updated: 2025/07/26 18:26:07 by obensarj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes_bonus/minishell_bonus.h"
 
+static int	_check_bonus_part(t_ast *node, t_shell *shell);
+
 int	executor(t_ast *node, t_shell *shell)
 {
-	pid_t	pid;
-	int		status;
-
 	if (!node)
 		return (shell->exit_status);
 	if (node->type == AST_REDIR)
 		return (exec_redirection(node, shell));
 	else if (node->type == AST_PIPE)
 		return (exec_pipe(node, shell));
-	else if (node->type == AST_AND)
-	{
-		status = executor(node->left, shell);
-		if (!status)
-			status = executor(node->right, shell);
-		return (status);
-	}
-	else if (node->type == AST_OR)
-	{
-		status = executor(node->left, shell);
-		if (status != 0)
-			status = executor(node->right, shell);
-		return (status);
-	}
-	else if (node->type == AST_SUBSHELL && node->left)
-	{
-		pid = fork();
-		if (pid == -1)
-			return (perror("subshell fork"), 1);
-		if (!pid)
-			exit(executor(node->left, shell));
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-		return (1);
-	}
+	else if (node->type == AST_AND || node->type == AST_OR)
+		return (_check_bonus_part(node, shell));
 	else if (node->type == AST_CMD && node->args)
 	{
 		if (_is_local_vars(node))
@@ -59,4 +34,26 @@ int	executor(t_ast *node, t_shell *shell)
 			return (exec_external(node, shell));
 	}
 	return (1);
+}
+
+static int	_check_bonus_part(t_ast *node, t_shell *shell)
+{
+	int	status;
+
+	if (node->type == AST_AND)
+	{
+		status = executor(node->left, shell);
+		if (!status)
+			status = executor(node->right, shell);
+	}
+	else if (node->type == AST_OR)
+	{
+		status = executor(node->left, shell);
+		if (status != 0)
+			status = executor(node->right, shell);
+	}
+	else
+		status = 1;
+	shell->exit_status = status;
+	return (status);
 }
