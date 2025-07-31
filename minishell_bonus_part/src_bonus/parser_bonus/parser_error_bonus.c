5a51d8072b3eb9a4125f9299394a3fe16ae9d2a1
Mohamed El Hamidi <mel-hami@e3r9p2.1337.ma>
2025-07-31 16:59:10 +0100

    update parser_error_bonus.c

--- minishell_bonus_part/src_bonus/parser_bonus/parser_error_bonus.c
+++ minishell_bonus_part/src_bonus/parser_bonus/parser_error_bonus.c
@@ -12,50 +12,101 @@
 
 #include "../../includes_bonus/minishell_bonus.h"
 
-static int	is_control_operator(t_token_type type)
+static int	is_invalid_edge_tokens(t_list *tokens)
 {
-	return (type == TOKEN_PIPE || type == TOKEN_AND || type == TOKEN_OR);
+	t_token	*first;
+	t_token	*last;
+
+	first = (t_token *)tokens->content;
+	last = (t_token *)ft_lstlast(tokens)->content;
+	if (is_control_operator(first->type) || first->type == TOKEN_P_CLOSE)
+		return (print_syntax_error(first->value));
+	if (is_control_operator(last->type) || last->type == TOKEN_P_OPEN)
+		return (print_syntax_error(first->value));
+	return (0);
+}
+
+static int	check_parenthesis_open(t_list *curr, t_list *prev)
+{
+	t_token	*token;
+	t_token	*next;
+
+	token = (t_token *)curr->content;
+	if (!curr->next)
+		return (print_syntax_error(token->value));
+	else if (prev && !is_control_operator(((t_token *)prev->content)->type)
+		&& ((t_token *)prev->content)->type != TOKEN_P_OPEN)
+		return (print_syntax_error(token->value));
+	next = (t_token *)curr->next->content;
+	if (next->type == TOKEN_P_CLOSE || is_control_operator(next->type))
+		return (print_syntax_error(next->value));
+	return (0);
+}
+
+static int	check_parenthesis_close(t_list *curr, t_list *prev)
+{
+	t_token	*token;
+	t_token	*next;
+
+	token = (t_token *)curr->content;
+	if (prev && is_control_operator(((t_token *)prev->content)->type))
+		return (print_syntax_error(token->value));
+	else if (!curr->next)
+		return (0);
+	next = (t_token *)curr->next->content;
+	if (next->type == TOKEN_P_OPEN || next->type == TOKEN_WORD)
+		return (print_syntax_error(next->value));
+	return (0);
+}
+
+static int	check_error(t_list *curr, t_list *prev, int *level)
+{
+	t_token	*token;
+
+	token = (t_token *)curr->content;
+	if (token->type == TOKEN_P_OPEN)
+	{
+		(*level)++;
+		return (check_parenthesis_open(curr, prev));
+	}
+	else if (token->type == TOKEN_P_CLOSE)
+	{
+		(*level)--;
+		if (*level < 0)
+			return (print_syntax_error(token->value));
+		return (check_parenthesis_close(curr, prev));
+	}
+	else if (is_control_operator(token->type) && prev
+		&& is_control_operator(((t_token *)prev->content)->type))
+		return (print_syntax_error(token->value));
+	else if (is_redirection(token->type) && !token->value)
+		return (SNTX_EXIT_STATUS);
+	return (0);
 }
 
 int	syntax_error(t_list *tokens)
 {
 	t_list	*curr;
 	t_list	*prev;
+	int		paren_level;
 
 	if (!tokens)
 		return (1);
-	if (is_control_operator(((t_token *)tokens->content)->type) || \
-		is_control_operator(((t_token *)ft_lstlast(tokens)->content)->type))
-		return (print_syntax_error(((t_token *)tokens->content)->value));
-	if (((t_token *)tokens->content)->type == TOKEN_P_CLOSE || \
-		((t_token *)ft_lstlast(tokens)->content)->type == TOKEN_P_OPEN)
-		return (print_syntax_error(((t_token *)tokens->content)->value));
-	prev = NULL;
+	if (is_invalid_edge_tokens(tokens))
+		return (SNTX_EXIT_STATUS);
 	curr = tokens;
+	prev = NULL;
+	paren_level = 0;
 	while (curr)
 	{
-		if (is_control_operator(((t_token *)curr->content)->type) && \
-			prev && is_control_operator(((t_token *)prev->content)->type))
-			return (print_syntax_error(((t_token *)curr->content)->value));
-		else if (is_redirection(((t_token *)curr->content)->type)
-			&& !((t_token *)curr->content)->value)
+		if (check_error(curr, prev, &paren_level))
 			return (SNTX_EXIT_STATUS);
 		prev = curr;
 		curr = curr->next;
 	}
-	return (0);
-}
-
-int	syntax_error_ast(t_ast *node)
-{
-	if (!node)
-		return (1);
-	if (node->type == AST_CMD)
-		return (!node->args || !node->args[0]);
-	else if (node->type == AST_PIPE)
-		return (!node->left || !node->right
-			|| syntax_error_ast(node->left) || syntax_error_ast(node->right));
-	else if (node->type == AST_REDIR)
-		return (!node->redir_file);
+	if (paren_level != 0)
+		return (
+			ft_putstr_fd("minishell: syntax error: unmatched parenthesis!\n",
+				STDERR_FILENO), SNTX_EXIT_STATUS);
 	return (0);
 }
