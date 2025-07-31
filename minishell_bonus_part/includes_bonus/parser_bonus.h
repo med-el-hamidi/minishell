ee20b5c73e743124b9ebabd63dcbb1f7a2285a77
Mohamed El Hamidi <mel-hami@e3r9p2.1337.ma>
2025-07-31 17:00:38 +0100

    update minishell_bonus_part/includes_bonus/parser_bonus.h

--- minishell_bonus_part/includes_bonus/parser_bonus.h
+++ minishell_bonus_part/includes_bonus/parser_bonus.h
@@ -16,10 +16,10 @@
 t_ast	*new_ast_node(int type, char **args);
 void	advance_token(t_list **tokens);
 int		is_redirection(int type);
+int     is_control_operator(t_token_type type);
 int		is_special(int type);
 
 int		syntax_error(t_list *tokens);
-int		syntax_error_ast(t_ast *node);
 t_ast	*parse_redirection(t_list **tokens, t_ast *command);
 t_ast	*parse_leading_redirection(t_list **tokens);
 t_ast	*parse_command(t_list **tokens);
